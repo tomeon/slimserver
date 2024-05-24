9bdcc1a83683c7e244ac747919ce6efdb2d19982
Matt Schreiber <tomeon@dogea.red>
2024-05-24 10:16:01 -0400

    Make smoke test easier to run locally
    by randoming the slimserver HTTP port.  `act` runs with `--network host`
    by default, meaning that the use of a hardcoded port number is liable to
    run into port-binding conflicts.
    
    Additional changes include:
    
        1. Fixing shellcheck linting issues,
        2. Making the script run under "strict mode" (`set -euo pipefail`),
        3. Retrying the JSON RPC request,
        4. Removing code that treats the output of `fgrep -q` (always the
           empty string) as an exit status,
        5. Making it possible to run the test script anywhere within the Git
           working tree, and
        6. Ensuring that `NODE_PID` contains an integer greater than 1
           before running `kill -9 "$NODE_PID"`.

--- t/00_smoketest.sh
+++ t/00_smoketest.sh
@@ -1,41 +1,93 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 SERVER_LOG=$HOME/server.log
 TIMEOUT=30
 
-function wait_port() {
-	local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+function retry {
+	local -i wait_seconds
 
-	until test $((wait_seconds--)) -eq 0 ; do
-		if nc -z localhost 9000 ; then
-			break
-		else
-			sleep 1
-		fi
+	if [[ "${1:--}" = - ]]; then
+		wait_seconds=10
+	else
+		wait_seconds="$1"
+	fi
+
+	shift
+
+	until (( (wait_seconds--) == 0 )); do
+		"$@" && return
+		sleep 1
 	done
 
-	((++wait_seconds))
+	return 1
 }
 
-function finish {
-	kill -9 $NODE_PID 2> /dev/null
-	cat $SERVER_LOG
-	rm $SERVER_LOG
+function wait_port {
+	retry "${1:-}" nc -z localhost "${HTTPPORT:-9000}"
 }
+
+function wait_status() {
+	# shellcheck disable=SC2317
+	_wait_status() {
+		local status
+		status=$(curl -m1 -SsX POST -d '{"id":0,"params":["",["serverstatus"]],"method":"slim.request"}' "http://localhost:${HTTPPORT:-9000}/jsonrpc.js") || return
+		jq . <<<"$status" || return
+		jq --raw-output '
+			.result.version | if . == null then "JSON RPC call did not return the expected structure\n" | halt_error(1) else . end
+		' <<<"$status" 1>/dev/null
+	}
+
+	retry "${1:-}" _wait_status
+}
+
+function random_tcp_port {
+	local -i port
+	while port="$RANDOM"; do
+		if (( port > 1024 )) && (( port < 65536 )) && ! nc -z localhost "$port"; then
+			printf -- '%d' "$port"
+			return
+		fi
+	done
+}
+
+# shellcheck disable=SC2317
+function finish {
+	if [[ -n "${NODE_PID:-}" ]] && (( NODE_PID > 1 )); then
+		kill -9 "$NODE_PID" 2> /dev/null || :
+		wait "$NODE_PID" || :
+	fi
+
+	cat "$SERVER_LOG" || :
+	rm -f "$SERVER_LOG"
+}
+
 trap finish EXIT
 
-./slimserver.pl --logfile=$SERVER_LOG &
-NODE_PID=$!
+if ! toplevel="$(git rev-parse --path-format=absolute --show-toplevel 2>/dev/null)"; then
+	toplevel_rel="${BASH_SOURCE[0]}/../.."
+	if ! toplevel="$(readlink -f "$toplevel_rel" 2>/dev/null)"; then
+		if ! toplevel="$(realpath "$toplevel_rel" 2>/dev/null)"; then
+			toplevel="${PWD:-$(pwd)}"
+		fi
+	fi
+fi
 
-wait_port $TIMEOUT || {
+HTTPPORT="$(random_tcp_port)"
+
+"${toplevel}/slimserver.pl" --httpport="$HTTPPORT" --logfile="$SERVER_LOG" &
+NODE_PID="$!"
+
+rc=0
+wait_port "$TIMEOUT" || {
+	rc="$?"
 	echo "Timing out trying to connect to LMS"
-	exit 1;
+	exit "$rc"
 }
 
-STATUS=$(curl -m1 -sX POST -d '{"id":0,"params":["",["serverstatus"]],"method":"slim.request"}' http://localhost:9000/jsonrpc.js)
-
-echo $STATUS | jq .
-
-RESULT=$(echo $STATUS | fgrep -q version)
-
-exit $RESULT
+wait_status "$TIMEOUT" || {
+	rc="$?"
+	echo "Timing out trying to get an API response from LMS"
+	exit "$rc"
+}
